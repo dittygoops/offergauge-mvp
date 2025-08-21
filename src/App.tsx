f30cb8eaf66d91422ea0bdc8bea5610d6ef45e94
dittygoops <aditya.gup16@gmail.com>
2025-08-20 20:18:35 -0700

    theme variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="text-3xl font-bold underline">Hello World</div>;
+  return <div className="text-teal-dark text-3xl font-bold">Hello World</div>;
 }
 
 export default App;
